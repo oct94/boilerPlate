8a4433e298839b63f4ac318a6cba24feb4b514e4
chaeheetae <chaeheetae94@gmail.com>
2021-10-12 01:18:59 +0900

    비밀정보 보호

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://oct94:gmlxo05061011@bilerplate.j1mmk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userShema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   name: {
     type: String,
     maxlength: 50,
